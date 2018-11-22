ccffa6554596f23b8b2e2722f73676fcf7b1bd6e
mhmdtshref <mhmdtshref@gmail.com>
2018-11-22 07:13:41 +0200

    prepare-to-upload #20

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
+var proccess = require('dotenv/config');
 const http=require('http');
 const router=require('./router.js');
-const PORT=3001;
-const server=http.createServer(router)
-server.listen(PORT);
-console.log(` the server up and running on port ${PORT}!`);
+//const PORT = ;
+const server=http.createServer(router);
+server.listen(process.env.PORT || 5000);
+console.log(` the server up and running on port ${process.env.PORT || 5000}!`);
