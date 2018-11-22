fd741865ced8e48377c96c2565215a2b844dd80f
mhmdtshref <mhmdtshref@gmail.com>
2018-11-22 11:32:13 +0200

    update-variables #32

--- src/test/tests.js
+++ src/test/tests.js
@@ -3,21 +3,21 @@ const searchCars = require('../searchCars/searchCars');
 
 
 
-test('tape working', function (t) {
+test('tape working', (t) => {
     const actual = 5;
     const expected = 5;
     t.equal(actual, expected, 'acctual and expected is not 5!');
     t.end();
 });
 
-test('try search for 1 result', function (t) {
+test('try search for 1 result', (t) => {
     const actual = searchCars('Abbot');
     const expected = ["Abbott-Detroit"];
     t.deepEqual(actual, expected, 'actual and expected is not 5!');
     t.end();
 });
 
-test('check limiting to first 5 elements', function (t) {
+test('check limiting to first 5 elements', (t) => {
     const actual = searchCars('ba');
     const expected = ['BAC', 'Baojun', 'BAIC Motor', 'JBA Motors', 'Abadal'];
     t.deepEqual(actual, expected, 'expected : ["BAC", "Baojun", "BAIC Motor", "Bentley", "Berkeley"]');
